85bcccddd0e2d4745c741c20045082a65679c11c
Giovanni Pellizzoni <giopellizzoni@outlook.com>
2025-02-05 18:50:43 +0100

    feat(shared): add global usings for ASP.NET Core components and data annotations

--- src/Shared/Shared/GlobalUsings.cs
+++ src/Shared/Shared/GlobalUsings.cs
@@ -1,7 +1,13 @@
+global using System.ComponentModel.DataAnnotations;
+
 global using MediatR;
 
 global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Diagnostics;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
 
 global using Shared.Data.Seed;
