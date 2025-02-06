0ec6758564a4a1c05ee52a777862fefd6c0f1e0c
Giovanni Pellizzoni <giopellizzoni@outlook.com>
2025-02-06 09:51:50 +0100

    feat(basket): add ShoppingCart and ShoppingCartItem models for basket functionality

---
+++ src/Modules/Basket/Basket/Basket/Models/ShoppingCart.cs
@@ -0,0 +1,54 @@
+namespace Basket.Basket.Models;
+
+public class ShoppingCart : Aggregate<Guid>
+{
+    public string UserName { get; private set; } = default!;
+
+    private readonly List<ShoppingCartItem> _items = new();
+
+    public IReadOnlyList<ShoppingCartItem> Items => _items.AsReadOnly();
+
+    public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+    public static ShoppingCart Create(
+        Guid id,
+        string userName)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(userName);
+        var shoppingCart = new ShoppingCart
+        {
+            Id = id,
+            UserName = userName
+        };
+
+        return shoppingCart;
+    }
+
+    public void AddItem(
+        Guid productId,
+        int quantity,
+        string color,
+        decimal price,
+        string productName)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var existingItem = Items.FirstOrDefault(x => x.ProductId == productId);
+        if (existingItem is not null)
+        {
+            existingItem.Quantity += quantity;
+        }
+        else
+        {
+            var newItem = new ShoppingCartItem(
+                Id,
+                productId,
+                quantity,
+                color,
+                price,
+                productName);
+            _items.Add(newItem);
+        }
+    }
+}
---
+++ src/Modules/Basket/Basket/Basket/Models/ShoppingCartItem.cs
@@ -0,0 +1,31 @@
+namespace Basket.Basket.Models;
+
+public class ShoppingCartItem : Entity<Guid>
+{
+    public ShoppingCartItem(Guid shoppingCartId,
+        Guid productId,
+        int quantity,
+        string color,
+        decimal price,
+        string productName)
+    {
+        ShoppingCartId = shoppingCartId;
+        ProductId = productId;
+        Quantity = quantity;
+        Color = color;
+        Price = price;
+        ProductName = productName;
+    }
+
+    public Guid ShoppingCartId { get; private set; } = default!;
+
+    public Guid ProductId { get; private set; } = default!;
+
+    public int Quantity { get; set; } = default;
+
+    public string Color { get; private set; } = default!;
+
+    public decimal Price { get; private set; } = default!;
+
+    public string ProductName { get; private set; } = default!;
+}
